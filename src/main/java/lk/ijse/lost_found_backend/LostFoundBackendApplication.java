a8486131735b364defcf5c0795827c61b5c2e885
KL193 <lakshankasthuriarachchi@gmail.com>
2025-05-29 20:35:14 +0530

    Intialize the backend

---
+++ src/main/java/lk/ijse/lost_found_backend/LostFoundBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.lost_found_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostFoundBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostFoundBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/lost_found_backend/LostFoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.lost_found_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFoundBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
